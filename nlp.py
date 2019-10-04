77b205330b0e3ec540e132bfd386a05224ec10e0
KimHS0915 <remember2552@gmail.com>
2019-10-04 15:22:04 +0900

    fix : i -> str(i)

--- nlp.py
+++ nlp.py
@@ -6,7 +6,7 @@ def tokenize(doc):
     okt = Okt()
     x = []
     for i in doc:
-        tokens = okt.pos(i, norm=True, stem=True)
+        tokens = okt.pos(str(i), norm=True, stem=True)
         y = []
         for j in tokens:
             y.append(j[0])
@@ -15,12 +15,9 @@ def tokenize(doc):
 
 def nlp(input_file, output_file, delimiter='\t', header=0, key='document', label='label'):
     data = pd.read_csv(input_file, delimiter=delimiter, header=header)
-    doc = data[key][:10]
-    print(len(doc))
+    doc = data[key]
     result = tokenize(doc)
-    print(len(result))
-    lab = tuple(data[label][:10])
-    print(len(lab))
+    lab = tuple(data[label])
     f = open(output_file, 'w')
     for i in range(len(lab)):
         dic = {'document':result[i], 'label':lab[i]}
