16c2af55f099606637f3894313abbbd767f063d0
KimHS0915 <remember2552@gmail.com>
2019-10-06 22:10:43 +0900

    Update Word2Vec

--- nlp.py
+++ nlp.py
@@ -1,6 +1,7 @@
 from konlpy.tag import Okt
 import pandas as pd
 import json
+from gensim.models import Word2Vec
 
 def tokenize(doc):
     okt = Okt()
@@ -13,10 +14,13 @@ def tokenize(doc):
         x.append(y)
     return x
 
-def nlp(input_file, output_file, delimiter='\t', header=0, key='document', label='label'):
+def nlp(input_file, output_file, delimiter='\t', header=0, key='document', label='label', vec=False):
     data = pd.read_csv(input_file, delimiter=delimiter, header=header)
     doc = data[key]
     result = tokenize(doc)
+    if vec==True:
+        model = Word2Vec(result, sg=1)
+        model.save("word2vec.model")
     lab = tuple(data[label])
     f = open(output_file, 'w')
     for i in range(len(lab)):
@@ -26,5 +30,5 @@ def nlp(input_file, output_file, delimiter='\t', header=0, key='document', label
         f.write('\n')
     f.close()
 
-nlp('ratings_train.txt', 'ratings_train.json')
+nlp('ratings_train.txt', 'ratings_train.json', vec=True)
 nlp('ratings_test.txt', 'ratings_test.json')
